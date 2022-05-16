abb5e59bd21c59923d4de9059d47e03fd51681f7
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2022-05-17 04:44:01 +0530

    Create WorkingWithNumbers.cs

---
+++ WorkingWithNumbers.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int num = 6;
+			num++;
+			Console.WriteLine(5.0 + 8.1);
+			Console.WriteLine(5 + 8.1);
+			Console.WriteLine(5 / 2.0);
+			Console.WriteLine(num);
+			num--;
+			Console.WriteLine(num);
+			Console.WriteLine(Math.Abs(-40));
+			Console.WriteLine(Math.Pow(3, 2));
+			Console.WriteLine(Math.Sqrt(36));
+			Console.WriteLine(Math.Max(4, 90));
+			Console.WriteLine(Math.Min(4, 90));
+			Console.WriteLine(Math.Round(69.69));
+		}
+	}
+}
