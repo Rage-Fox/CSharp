77f94a5cdb17135ba72aad9e45b234d8e28b76cb
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2022-05-16 20:01:44 +0530

    Create HelloWorldAndBasicPrinting.cs

---
+++ HelloWorldAndBasicPrinting.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{//denotes program is starting
+    class Program//container where stores bunch of code
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("    /|");
+            Console.WriteLine("   / |");
+            Console.WriteLine("  /  |");
+            Console.WriteLine(" /   |");
+            Console.WriteLine("/____|");
+            Console.ReadLine();//makes command prompt display until we press enter
+        }
+    }
+}
