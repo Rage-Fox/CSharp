8b684a70e5ef4ee69d4de4ef4e7d8ce95cfd1e66
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2022-05-17 20:50:36 +0530

    Create SwitchStatements.cs

---
+++ SwitchStatements.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(GetDay(dayNum));
+        }
+        static string GetDay(int dayNum)
+        {
+            string dayName;
+            switch(dayNum)
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+                case 4:
+                    dayName = "Thursday";
+                    break;
+                case 5:
+                    dayName = "Friday";
+                    break;
+                case 6:
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Invalid day";
+                    break;
+            }
+            return dayName;
+        }
+    }
+}
