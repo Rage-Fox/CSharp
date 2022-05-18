97f4c985e29f4d6c0401cc4f83c1ed8381c8ae51
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2022-05-18 06:09:58 +0530

    Create ClassBook.cs

---
+++ ClassBook.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Book//class is actually code for a new data type
+    {
+        public string title;
+        public string author;
+        public int pages;
+    }
+}
